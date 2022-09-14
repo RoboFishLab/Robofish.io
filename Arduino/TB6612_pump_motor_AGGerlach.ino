35313492667da6721a1558200668357876eab614
Fritz <fritz.a.francisco@gmail.com>
2022-09-14 17:58:48 +0200

    updated documentation

--- Arduino/TB6612_pump_motor_AGGerlach.ino
+++ Arduino/TB6612_pump_motor_AGGerlach.ino
@@ -1,8 +1,7 @@
     /*
-  TB6612FNG-Dual-Driver
-  made on 28 oct 2020
-  by Amir Mohammad Shojaee @ Electropeak
-  Home
+  TB6612_pump_motor_AGGerlach
+  made on 14 sep 2022
+  by Fritz Francisco @ HU, Berlin
 
   Tested with ESP32 WROOM32, TB6612, ESP32 Dev Modeule
 */
@@ -10,12 +9,12 @@
 
 #include <analogWrite.h>
 
-#define AIN1 27
-#define AIN2 26
-#define PWM1 14
-#define LED 22
-#define SEND 21
-#define RECEIVE 12
+#define AIN1 27 // define pin id number
+#define AIN2 26 // define pin id number
+#define PWM1 14 // define pin id number
+#define LED 22 // define pin id number
+#define SEND 21 // define pin id number
+#define RECEIVE 12 // define pin id number
 
 int pmwvalue;
 int ledOn;
@@ -28,12 +27,12 @@ void setup() {
   }
   Serial.println("Setup");
 
-  pinMode(PWM1,OUTPUT);
-  pinMode(AIN1,OUTPUT);
-  pinMode(AIN2,OUTPUT);
-  pinMode(LED,OUTPUT);
-  pinMode(SEND,OUTPUT);
-  pinMode(RECEIVE,INPUT);
+  pinMode(PWM1,OUTPUT); // Set pin modes
+  pinMode(AIN1,OUTPUT); // Set pin modes
+  pinMode(AIN2,OUTPUT); // Set pin modes
+  pinMode(LED,OUTPUT); // Set pin modes
+  pinMode(SEND,OUTPUT); // Set pin modes
+  pinMode(RECEIVE,INPUT); // Set pin modes
 
   digitalWrite(AIN1,HIGH); //Motor A Rotate Clockwise
   digitalWrite(AIN2,LOW);
@@ -43,15 +42,16 @@ void setup() {
 }
 
 void loop() {
+// Switch logic, if switch is on RECEIVE pin receives load and ledOn is set to HIGH
 ledOn=digitalRead(RECEIVE);
 if (ledOn==HIGH){
-  pmwvalue = 0.8 * 255; // max 255
-  digitalWrite(SEND,LOW);
-  digitalWrite(LED,HIGH);
+  pmwvalue = 0.55 * 255; // percentage of power supplied to motor, max: 255
+  digitalWrite(SEND,LOW); 
+  digitalWrite(LED,HIGH); // Turn on LED
 }
 else{
   pmwvalue=0;  
-  digitalWrite(LED,LOW);
+  digitalWrite(LED,LOW); // Turn off LED
 }
 analogWrite(PWM1,pmwvalue);
 }
